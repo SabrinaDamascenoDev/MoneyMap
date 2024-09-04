eedaee95ba96b043a3e025282673cfcd34a7e69b
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-09-04 19:47:16 -0300

    comeco da estilizacao da parte inicial do app

--- MoneyMap/src/componentes/Inicial/Inicial.js
+++ MoneyMap/src/componentes/Inicial/Inicial.js
@@ -1,18 +1,47 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const Page1 = () => (
   <View style={styles.container}>
-    <Text>Page 1</Text>
+    <Text style={styles.logo}>LOGO</Text>
+    <Image
+      source={require('../../Imagens/imagemInicial.png')} // Caminho para o arquivo PNG
+      style={styles.image}
+    />
+    <Text style={styles.titleInicio}>Assuma o Controle Total das Suas Finanças.</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+   
     alignItems: 'center',
   },
+  image: {
+    width: 300, // Largura da imagem
+    height: 200,
+    justifyContent: 'top', 
+    alignItems: 'top',
+    marginTop: 20,
+    
+  },
+  titleInicio: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    paddingLeft:20,
+    paddingRight: 20,
+    textAlign: 'center',
+    marginTop: 25
+  },
+
+  logo:{
+    marginTop: 100,
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'left',
+  }
+
 });
 
 export default Page1;
