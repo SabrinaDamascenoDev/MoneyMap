9546087e5719798da38d2b370019cc9af7643198
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-09-13 08:15:45 -0300

    instalando dependencias firebase

--- MoneyMap/App.js
+++ MoneyMap/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import Routes from './src/componentes/Routes/Routes';
 import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC8OkMY51zv_7o6YfLaplHoUmoeQUnFI_E",
+  authDomain: "moneymap-198ca.firebaseapp.com",
+  projectId: "moneymap-198ca",
+  storageBucket: "moneymap-198ca.appspot.com",
+  messagingSenderId: "922708214331",
+  appId: "1:922708214331:web:7183d159c575f4d892121e",
+  measurementId: "G-23ZM98M69H"
+
+};
+
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
